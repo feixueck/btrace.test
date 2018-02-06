8b6cf03c758553c53f55e12aadd7ae4972ccf1f6
zuoshuaiok <zuoshuaiok@126.com>
2018-02-06 10:13:05 +0800

    for compile errro

--- src/main/java/com/zuosh/bt/Test.java
+++ src/main/java/com/zuosh/bt/Test.java
@@ -1,13 +1,14 @@
 package com.zuosh.bt;
 
 import com.sun.btrace.annotations.*;
-1
+
 import static com.sun.btrace.BTraceUtils.println;
 
 /**
  * Created by zuoshuai on 2018/2/1.
  */
 @BTrace
+
 public class Test {
     @OnMethod(clazz = "com.bst.suit.web.action.base.user.UserInfoController"
             , method = "findLoginUser", location = @Location(Kind.RETURN))
